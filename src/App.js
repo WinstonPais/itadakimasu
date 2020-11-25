a3d3d0201582c6d202f49f0ab0bf5973307d189f
WinstonPais <51374812+WinstonPais@users.noreply.github.com>
2020-11-25 18:00:44 +0530

    initial Commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import classes from './App.module.css';
+
+class App extends Component {
+  render(){
+    return(
+      <div className={classes.App}>
+        <h1>Hello World</h1>
+      </div>
+    )
+  }
+}
+
+export default App;
