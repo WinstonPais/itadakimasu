78dcdf16e4bfa0e45131c7c98bfc9685864a37ab
WinstonPais <51374812+WinstonPais@users.noreply.github.com>
2020-11-25 19:45:49 +0530

    Initialized RecipeCard Component
    Created Dummy html and css files for testing in test folder
    
    made card with background picture, non-veg/veg mark and the translucent bottom half

---
+++ src/RecipeCard/RecipeCard.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import classes from './RecipeCard.module.css';
+
+const recipeCard = ( props ) => {
+    return(
+        <div>
+            
+        </div>
+    )
+}
+
+export default recipeCard;
