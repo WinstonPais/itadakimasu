0444981d30f0682dccecbe23bacad070fea6c0c2
WinstonPais <51374812+WinstonPais@users.noreply.github.com>
2020-12-17 13:58:13 +0530

    CI/CD tests

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import classes from './App.module.css';
+// import classes from './App.module.css';
 import RecipeCard from './RecipeCard/RecipeCard'
 
 class App extends Component {
