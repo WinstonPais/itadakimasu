8473a086c8638a1c6c67b9a3d71eae958dcef2f1
Wilton <wiltondsouza100@gmail.com>
2020-12-30 13:57:38 +0530

    width and height updated, removal of autoplayspeed

--- src/footer/images/images.js
+++ src/footer/images/images.js
@@ -7,11 +7,12 @@ import sushi from './sushi.jpg';
 import noodles from './noodles.jpg';
 import Carousel from 'react-multi-carousel';
 import 'react-multi-carousel/lib/styles.css';
+import classes from './images.module.css';
 
 const responsive = {
   desktop: {
     breakpoint: { max: 3000, min: 1024 },
-    items: 6
+    items: 5
   },
   tablet: {
     breakpoint: { max: 1024, min: 464 },
@@ -25,13 +26,13 @@ const responsive = {
 
 function Imageslist() {
   return(
-    <Carousel autoPlay={true} infinite={true} autoPlaySpeed={1000} responsive={responsive}>
-      <img src={pizza} alt="pizza" />
-      <img src={burger} alt="burger" />
-      <img src={roll} alt="roll" />
-      <img src={beef} alt="beef" />
-      <img src={sushi} alt="sushi" />
-      <img src={noodles} alt="noodles" />
+    <Carousel infinite={true} autoPlay={true} responsive={responsive}>
+      <img className={classes.imag} src={pizza} alt="pizza" />
+      <img className={classes.imag} src={burger} alt="burger" />
+      <img className={classes.imag} src={roll} alt="roll" />
+      <img className={classes.imag} src={beef} alt="beef" />
+      <img className={classes.imag} src={sushi} alt="sushi" />
+      <img className={classes.imag} src={noodles} alt="noodles" />
     </Carousel>
   );  
 }
