c37d8b52fa43b84b04807f531d728d9bc746e4ba
WinstonPais <51374812+WinstonPais@users.noreply.github.com>
2020-12-31 00:34:45 +0530

    Update Grid.js

--- src/Grid/Grid.js
+++ src/Grid/Grid.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import { Row, Col } from 'reactstrap';
 import RecipeCard from './../RecipeCard/RecipeCard';
 import classes from './Grid.module.css';
