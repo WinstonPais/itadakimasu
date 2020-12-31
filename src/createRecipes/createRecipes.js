141b7d00265b5afd6bb9cf00a304b69a1b173529
WinstonPais <51374812+WinstonPais@users.noreply.github.com>
2020-12-31 22:53:49 +0530

    fix unused imports

--- src/createRecipes/createRecipes.js
+++ src/createRecipes/createRecipes.js
@@ -1,5 +1,5 @@
-import React, { Component, Fragment } from 'react';
-import { Row,Container } from 'reactstrap';
+import React, { Component} from 'react';
+import { Container } from 'reactstrap';
 import createbg from './createbg.png';
 import classes from './createRecipes.module.css';
 
@@ -7,14 +7,10 @@ class CreateRecipes extends Component {
     render(){
         return(
             <div className={classes.bm}>
-            {/* // <Row className={classes.bm}>
-                //  <Container className={[classes.container1,classes.bm].join(' ')}> */}
-                    <img className={classes.ig} src={createbg} alt="CreateRecipesbg" />
-                    <Container>
-                        <div className={classes.centered}><p>Create your Perfect</p><p>Recipes and Dishes</p></div>
-                    </Container>
-                {/* // </Container>
-            // </Row> */}
+                <img className={classes.ig} src={createbg} alt="CreateRecipesbg" />
+                <Container>
+                    <div className={classes.centered}><p>Create your Perfect</p><p>Recipes and Dishes</p></div>
+                </Container>
             </div>
         )
     }
