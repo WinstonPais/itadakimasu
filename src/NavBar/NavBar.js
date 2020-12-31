ba1a0c389e22761d11487725e52f0a7a29518f88
WinstonPais <51374812+WinstonPais@users.noreply.github.com>
2020-12-31 23:54:04 +0530

    initial firebase auth

--- src/NavBar/NavBar.js
+++ src/NavBar/NavBar.js
@@ -10,10 +10,59 @@ import {
   } from 'reactstrap';
 import NavBarLogo from './NavBarLogo.png';
 import classes from './NavBar.module.css';
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import firebaseConfig from './../firebase.config';
 
 const NavBar = ( props ) => {
     const [isOpen, setIsOpen] = useState(false);
     const [navbar, setNavBar] = useState(false);
+    const [isUserAuth, setIsUserAuth] = useState(false);
+
+    console.log(firebaseConfig);
+    const logMeIn = () => {
+
+      var provider = new firebase.auth.GoogleAuthProvider();
+      firebase.auth()
+        .signInWithPopup(provider)
+        .then((result) => {
+          /** @type {firebase.auth.OAuthCredential} */
+          // var credential = result.credential;
+
+          // This gives you a Google Access Token. You can use it to access the Google API.
+          // var token = credential.accessToken;
+          // The signed-in user info.
+          var user = result.user;
+          console.log(user);
+          setIsUserAuth(true);
+          // ...
+        }).catch((error) => {
+          // Handle Errors here.
+          const errorCode = error.code;
+          const errorMessage = error.message;
+          // The email of the user's account used.
+          // var email = error.email;
+          // The firebase.auth.AuthCredential type that was used.
+          // var credential = error.credential;
+          // ...
+          alert(errorMessage);
+          console.log(errorCode);
+        });
+    }
+
+    const logMeOut = () => {
+      firebase.auth().signOut().then(() => {
+        // Sign-out successful.
+        setIsUserAuth(false);
+      }).catch((error) => {
+        // An error happened.
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        alert(errorMessage);
+        console.log(errorCode);
+
+      });
+    }
 
     const changeBackground = () =>{
         if (window.scrollY >= 80) {
@@ -27,11 +76,14 @@ const NavBar = ( props ) => {
     window.addEventListener('scroll', changeBackground)
 
     const toggle = () => setIsOpen(!isOpen);
+
+    const loginButton = <Button className={classes.buttonLogin} onClick={logMeIn} color="warning">Login</Button>;
+    const LogoutButton = <Button className={classes.buttonLogin} onClick={logMeOut} color="warning">LogOut</Button>;
+    
     return(
         <Fragment>
       <Navbar fixed="top" className={navbar ? [classes.navbarcustom,classes.navbarActive].join(' ') : classes.navbarcustom} dark expand="md">
-        {/* <NavbarBrand href="/">reactstrap</NavbarBrand> */}
-        <a className={"navbar-brand "+classes.anchorParent} href="https://www.google.com/">
+        <a className={"navbar-brand "+classes.anchorParent} href="https://winstonpais.github.io/itadakimasu/">
             <img src={NavBarLogo} width="70" height="70" className={"d-inline-block align-top "+ classes.imageMargin} alt=""/>
             <span className={[classes.anchorChildren,classes.anchorspan].join(' ')}>Itadakimasu</span>
         </a>
@@ -39,16 +91,16 @@ const NavBar = ( props ) => {
         <Collapse isOpen={isOpen} navbar>
           <Nav className={"ml-auto "+classes.MainNav} navbar>
             <NavItem>
-              <NavLink className={classes.linktextColor} href="https://www.google.com/">Home</NavLink>
+              <NavLink className={classes.linktextColor} href="https://winstonpais.github.io/itadakimasu/">Home</NavLink>
             </NavItem>
             <NavItem>
-              <NavLink className={classes.linktextColor} href="https://www.google.com/">Recipes</NavLink>
+              <NavLink className={classes.linktextColor} href="https://winstonpais.github.io/itadakimasu/">Recipes</NavLink>
             </NavItem>
             <NavItem>
-              <NavLink className={classes.linktextColor} href="https://www.google.com/">Contact</NavLink>
+              <NavLink className={classes.linktextColor} href="https://winstonpais.github.io/itadakimasu/">Contact</NavLink>
             </NavItem>
             <NavItem>
-                <Button className={classes.buttonLogin} color="warning">Login</Button>
+                { isUserAuth ? LogoutButton : loginButton }
             </NavItem>
           </Nav>
         </Collapse>
