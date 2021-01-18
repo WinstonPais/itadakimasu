f9f2143e7da7f234879a97f97fef06e2c393b384
Wilton <wiltondsouza100@gmail.com>
2021-01-18 15:55:02 +0530

    contactpage

--- src/MainPages/ContactPage/ContactPage.js
+++ src/MainPages/ContactPage/ContactPage.js
@@ -6,11 +6,11 @@ import Footer from './../../footer/footer';
 const ContactPage = () => {
     return (
         <>
-        <Container style={{position:'relative',marginTop:'20vh'}}>
+        <Container className={classes.mycont} style={{position:'relative',marginTop:'25vh',}}>
             <Row className={classes.myRow}>
                 <Col style={{backgroundColor: 'rgb(39, 209, 221)'}} className={classes.myCol}>
-                    <a href="https://github.com/WinstonPais/itadakimasu/issues">
-                        <div className={classes.cc} style={{height:'100%'}}>
+                    <a onClick={()=> window.open("https://github.com/WinstonPais/itadakimasu/issues","_blank")} >
+                        <div className={classes.center} style={{height:'100%'}}>
                             <div>
                                 <img alt="asg" style={{height:'200px',width:'200px'}} src="https://github.githubassets.com/images/modules/logos_page/Octocat.png" />
                                 <h3>Click Here to Report Issues</h3>
@@ -19,8 +19,9 @@ const ContactPage = () => {
                     </a>
                 </Col>
                 <Col style={{backgroundColor: 'rgb(32, 30, 30)'}} className={classes.myCol}>
-                    <div className={classes.cc} style={{height:'100%'}}>
-                    <h3 syle={{color:'white'}}>Contact Us</h3>
+                    <div className={classes.center} style={{height:'100%',margin:"auto",textAlign:"center",verticalAlign: 'middle'}}>
+                        <h1 style={{color:'white'}}>Contact Us</h1>
+                        <a href="mailto:wiltondsouza100@gmail.com">Send email</a>
                     </div>
                     
                 </Col>
