e54bc3e4e5e342b17ab4d6931daafb038913fc78
WinstonPais <51374812+WinstonPais@users.noreply.github.com>
2021-01-21 02:11:10 +0530

    Update RecipeDetailsPage.js

--- src/MainPages/RecipeDetailsPage/RecipeDetailsPage.js
+++ src/MainPages/RecipeDetailsPage/RecipeDetailsPage.js
@@ -135,7 +135,7 @@ const RecipeDetailsPage = () => {
                             <h3 style={{marginBottom:"1em"}}>Grocery Shops Close to me</h3>
                             <iframe
                             width="100%"
-                            height="100%"
+                            height="350"
                             frameborder="0" style={{border:"0"}}
                             src="https://www.google.com/maps/embed/v1/search?key=AIzaSyDwNoCvdev9LYCoC89gBc-ONHhjDXdh0Ik&q=grocery+shops+close+to+me" allowfullscreen>
                             </iframe>
